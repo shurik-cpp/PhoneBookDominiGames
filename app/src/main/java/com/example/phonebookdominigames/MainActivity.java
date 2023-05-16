e4cdc030cb3f3ce2660f3cd3a22311c1012c2997
shurik-cpp <shurik.cpp@gmail.com>
2023-05-16 21:33:08 +0700

    Inital commit

---
+++ app/src/main/java/com/example/phonebookdominigames/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.phonebookdominigames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.phonebookdominigames.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'phonebookdominigames' library on application startup.
+    static {
+        System.loadLibrary("phonebookdominigames");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'phonebookdominigames' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
