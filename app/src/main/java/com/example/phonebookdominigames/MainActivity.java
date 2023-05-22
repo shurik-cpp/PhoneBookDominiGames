baabe8d7fac7ad31797e6d2b56d7ad3c5589be9f
shurik-cpp <shurik.cpp@gmail.com>
2023-05-22 18:50:32 +0700

    WIP: Пощупал ListView (java)

--- app/src/main/java/com/example/phonebookdominigames/MainActivity.java
+++ app/src/main/java/com/example/phonebookdominigames/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.phonebookdominigames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.phonebookdominigames.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +27,20 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        ArrayList<Person> PhoneBookData = new ArrayList<Person>();
+        PhoneBookData.add(new Person("name", "number"));
+        PhoneBookData.add(new Person("name1", "number1"));
+        PhoneBookData.add(new Person("name2", "number2"));
+
+
+        ListView lv = binding.personsListView;
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, PhoneBookData);
+        lv.setAdapter(adapter);
+
         TextView tv = binding.sampleText;
-        tv.setText(GetPhoneBookDataFromJNI());
+        //tv.setText(GetPhoneBookDataFromJNI());
 
     }
 
---
+++ app/src/main/java/com/example/phonebookdominigames/Person.java
@@ -0,0 +1,12 @@
+package com.example.phonebookdominigames;
+
+import java.util.jar.Attributes;
+
+public class Person {
+    Person(String name, String number) {
+        Name = name;
+        Number = number;
+    }
+    public String Name;
+    public String Number;
+}
